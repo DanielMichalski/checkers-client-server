fa9e098970208996e8d402c3b0e96b10fa1977b7
bakeraw2 <michalskidaniel2@gmail.com>
2014-05-04 19:45:37 +0200

    Poprawki

--- src/main/java/com/ninjaapps/checkers/Runner.java
+++ src/main/java/com/ninjaapps/checkers/Runner.java
@@ -1,14 +1,16 @@
 package com.ninjaapps.checkers;
 
-import com.ninjaapps.checkers.view.game.new_code.view.GameFrame;
+import com.ninjaapps.checkers.util.ApplicationUtil;
+import com.ninjaapps.checkers.view.game.view.GameFrame;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Runner {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context_base.xml");
+        ApplicationUtil.setSystemLookAndFeel();
+
+        ApplicationContext context = ApplicationUtil.context;
 
         GameFrame gameFrame = context.getBean("gameFrame", GameFrame.class);
         gameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
--- src/main/java/com/ninjaapps/checkers/server/TCPServer.java
+++ src/main/java/com/ninjaapps/checkers/server/TCPServer.java
@@ -3,7 +3,6 @@ package com.ninjaapps.checkers.server;
 import com.ninjaapps.checkers.log.AutowiredLogger;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import com.ninjaapps.checkers.view.game.old.Pawns;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
@@ -45,57 +44,62 @@ public class TCPServer implements Runnable {
         }
     }
 
-    public Pawns listen() {
-        boolean flag = true;
-        Pawns p = null;
-        while (flag) {
-            try {
-                p = (Pawns) ois.readObject();
-                flag = false;
-            } catch (IOException e) {
-                flag = false;
-                Rozlacz();
-                logger.error("Error while listening clients. ", e);
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            } catch (ClassNotFoundException e) {
-                logger.error("Error while listening clients. ", e);
-                JOptionPane.showMessageDialog(null, e.getMessage());
-            }
-
-        }
-        return p;
-    }
-
-    public void send(Pawns p) {
-        try {
-            oos.writeObject(p);
-            oos.flush();
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-        }
-    }
-
+    @Override
     public void run() {
-        try {
-            ss = new ServerSocket(serverPort);
-            socket = ss.accept();
-            oos = new ObjectOutputStream(socket.getOutputStream());
-            ois = new ObjectInputStream(socket.getInputStream());
 
-        } catch (IOException ex) {
-            logger.error("Error ");
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-        }
     }
 
-    public void Rozlacz() {
-        try {
-            oos.close();
-            ois.close();
-            socket.close();
-            ss.close();
-        } catch (IOException e) {
-            logger.error("Błąd przy rozłączaniu servera.", e);
-        }
-    }
+//    public Pawns listen() {
+//        boolean flag = true;
+//        Pawns p = null;
+//        while (flag) {
+//            try {
+//                p = (Pawns) ois.readObject();
+//                flag = false;
+//            } catch (IOException e) {
+//                flag = false;
+//                Rozlacz();
+//                logger.error("Error while listening clients. ", e);
+//                JOptionPane.showMessageDialog(null, e.getMessage());
+//            } catch (ClassNotFoundException e) {
+//                logger.error("Error while listening clients. ", e);
+//                JOptionPane.showMessageDialog(null, e.getMessage());
+//            }
+//
+//        }
+//        return p;
+//    }
+//
+//    public void send(Pawns p) {
+//        try {
+//            oos.writeObject(p);
+//            oos.flush();
+//        } catch (IOException ex) {
+//            JOptionPane.showMessageDialog(null, ex.getMessage());
+//        }
+//    }
+//
+//    public void run() {
+//        try {
+//            ss = new ServerSocket(serverPort);
+//            socket = ss.accept();
+//            oos = new ObjectOutputStream(socket.getOutputStream());
+//            ois = new ObjectInputStream(socket.getInputStream());
+//
+//        } catch (IOException ex) {
+//            logger.error("Error ");
+//            JOptionPane.showMessageDialog(null, ex.getMessage());
+//        }
+//    }
+//
+//    public void Rozlacz() {
+//        try {
+//            oos.close();
+//            ois.close();
+//            socket.close();
+//            ss.close();
+//        } catch (IOException e) {
+//            logger.error("Błąd przy rozłączaniu servera.", e);
+//        }
+//    }
 }
